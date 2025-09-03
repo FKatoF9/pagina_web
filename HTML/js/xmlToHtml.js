2e0321d2d8a79717d635c721d6620732b0d9a9b3
katof9 <kmonroyf@unal.edu.co>
2025-09-03 17:39:22 -0500

    v 1.5

--- HTML/js/xmlToHtml.js
+++ HTML/js/xmlToHtml.js
@@ -1,4 +1,3 @@
-<script>
 function xml2html() {
     var xmlhttp;
     if (window.XMLHttpRequest) {
@@ -8,24 +7,26 @@ function xml2html() {
         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
 
-    xmlhttp.open("GET", "./xml/Factura.xml", false);
+    xmlhttp.open("GET", "../XML/Factura.xml", false);
     xmlhttp.send();
 
     var xmlDoc = xmlhttp.responseXML;
 
     // Manejo de errores si no carga el XML
     if (!xmlDoc) {
-        document.write("<p><strong>Error:</strong> No se pudo cargar o parsear el archivo XML.</p>");
+        document.getElementById("container").innerHTML = "<p><strong>Error:</strong> No se pudo cargar o parsear el archivo XML.</p>";
         return;
     }
 
     // Título
-    document.write("<h1>Mis Clientes (Receptores de Facturas)</h1>");
+    var html = "<h1>Mis Clientes (Receptores de Facturas)</h1>";
 
     // Tabla
-    document.write("<table border='1' style='color:black; width:100%; border-collapse: collapse;'>");
-    document.write("<tr style='background-color: #3498db; color: white; text-align: center;'>");
-    document.write("<th>Nombre</th><th>NIT</th><th>Dirección</th><th>Teléfono</th><th>Correo</th></tr>");
+    html += "<table border='1' style='color:black; width:100%; border-collapse: collapse;'>";
+
+    html += "<tr style='background-color: #3498db; color: white; text-align: center;'>";
+
+    html += "<th>Nombre</th><th>NIT</th><th>Dirección</th><th>Teléfono</th><th>Correo</th></tr>";
 
     // Obtener todas las facturas
     var facturas = xmlDoc.getElementsByTagName("factura");
@@ -41,18 +42,23 @@ function xml2html() {
             var telefono = getValor(receptor, "telefono");
             var correo = getValor(receptor, "correo");
 
-            document.write("<tr style='text-align: center;'>");
-            document.write("<td>" + nombre + "</td>");
-            document.write("<td>" + nit + "</td>");
-            document.write("<td>" + direccion + "</td>");
-            document.write("<td>" + telefono + "</td>");
-            document.write("<td>" + correo + "</td>");
-            document.write("</tr>");
+            html += "<tr style='text-align: center;'>";
+
+            html += "<td>" + nombre + "</td>";
+            html += "<td>" + nit + "</td>";
+            html += "<td>" + direccion + "</td>";
+            html += "<td>" + telefono + "</td>";
+            html += "<td>" + correo + "</td>";
+
+            html += "</tr>";
         }
     }
 
-    document.write("</table>");
+    html += "</table>";
 
+    // Insertar contenido en el contenedor
+    document.getElementById("container").innerHTML = html;
+}
 
 // Función auxiliar para obtener el valor de un elemento
 function getValor(parent, tagName) {
@@ -62,4 +68,3 @@ function getValor(parent, tagName) {
 
 // Llamar a la función cuando la página cargue
 window.onload = xml2html;
-</script>
